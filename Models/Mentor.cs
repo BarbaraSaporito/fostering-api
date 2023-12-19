ec7d18f9bac856c1473f0259d399a3c9cd882ca4
Barbara Saporito <aparecidasaporito@hotmail.com>
2023-12-19 15:47:54 -0300

    first commit

---
+++ Models/Mentor.cs
@@ -0,0 +1,26 @@
+﻿namespace FosteringAPI.Models
+{
+    public class Mentor
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int Limit { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Course { get; set; }
+
+        public string Photo { get; set; }
+
+        public string Sentence { get; set; }
+
+        public string Hobby { get; set; }
+
+        public string Period { get; set; }
+
+    }
+}
