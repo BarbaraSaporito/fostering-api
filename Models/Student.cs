caab00bd2a874bd8145b9558bc96ad351ed39f6a
Barbara Saporito <aparecidasaporito@hotmail.com>
2023-12-21 16:21:52 -0300

    Fixes on github branch

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,12 @@
 ﻿namespace FosteringAPI.Models
 {
     public class Student
-    {
+    {   public string Name { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Course { get; set; }
+        public string Period { get; set; }
+        public string Hobby { get; set; }
+        public string Sentence { get; set; }
     }
 }
